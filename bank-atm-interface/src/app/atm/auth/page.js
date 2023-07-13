6634d7fae3e7590641491a8aa921a4bea8837b25
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-07-13 15:33:33 +0000

    Error overlay added to atm pin entry page

--- bank-atm-interface/src/app/atm/auth/page.js
+++ bank-atm-interface/src/app/atm/auth/page.js
@@ -1,16 +1,44 @@
 "use client";
+import { DialogueBox } from "@/components";
+import { FilledButton } from "@/components/Buttons/Buttons";
 import { PinEntryPage } from "@/sharedPages";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [otp, setOtp] = useState("");
+  const [openDialogue, setOpenDialogue] = useState(false);
+
+  //TODO: Make API calls
+  const comparePIN = (val) => {
+    val === "1234" ? alert("correct PIN") : setOpenDialogue(true);
+  };
   return (
-    <PinEntryPage
-      heading="Please enter your PIN to proceed"
-      otp={otp}
-      setOtp={setOtp}
-      onComplete={(val) => alert(val)}
-    />
+    <>
+      <PinEntryPage
+        heading="Please enter your PIN to proceed"
+        otp={otp}
+        setOtp={setOtp}
+        onComplete={comparePIN}
+      />
+      <DialogueBox
+        open={openDialogue}
+        setOpen={setOpenDialogue}
+        heading="Invalid PIN"
+        body="Please try again. Or visit any of 
+our branches to reset your PIN."
+        footer={
+          <>
+            <FilledButton type="warning" onClick={() => setOpenDialogue(false)}>
+              {/**Todo: Redirect to initial page on click */}
+              Cancel
+            </FilledButton>
+            <FilledButton onClick={() => setOpenDialogue(false)}>
+              Retry
+            </FilledButton>
+          </>
+        }
+      />
+    </>
   );
 };
 
