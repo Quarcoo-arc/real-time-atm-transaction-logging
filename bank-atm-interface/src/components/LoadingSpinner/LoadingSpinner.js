0c3d71fd2fe7597ac482cec883da8e72068f49d1
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-08-16 20:40:57 +0000

    Changed the spinner component style

--- bank-atm-interface/src/components/LoadingSpinner/LoadingSpinner.js
+++ bank-atm-interface/src/components/LoadingSpinner/LoadingSpinner.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Backdrop from "@mui/material/Backdrop";
-import CircularProgress from "@mui/material/CircularProgress";
+import ClipLoader from "react-spinners/ScaleLoader";
 
 const LoadingSpinner = ({ open }) => {
   return (
     <Backdrop sx={{ color: "#E1D4BB", zIndex: 1500 }} open={open}>
-      <CircularProgress color="inherit" />
+      <ClipLoader
+        color="#E1D4BB"
+        loading={open}
+        cssOverride={{}}
+        size={300}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
     </Backdrop>
   );
 };
